16f87e5fb14881054803bce48624e96eb1b8f37b
eccentricayman <aahmed3@stuy.edu>
2017-05-28 02:11:11 -0400

    fixederino

--- utils/recognition.py
+++ utils/recognition.py
@@ -1,11 +1,8 @@
 from clarifai.rest import ClarifaiApp
 
-client_id = ""
-client_secret = ""
-with open("utils/client_secret.txt") as secrets_file:
-    data = secrets_file.read().split("\n")
-    client_id = data[0]
-    client_secret = data[1]
+secrets = getSecrets()
+client_id = secrets['client-id']
+client_secret = secrets['client-secret']
     
 #GITIGNORE THIS BOI
 app = ClarifaiApp(client_id, client_secret)
