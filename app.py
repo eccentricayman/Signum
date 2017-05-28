10bd760aac2344081ff54178305c0de7857d22cd
William Xiang <wxiang@stuy.edu>
2017-05-28 01:51:21 -0400

    updates

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 secrets = secrets.getSecrets()
 app.secret_key= secrets['session-key']
 
-app.config['UPLOAD_FOLDER'] = "uploads"
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+app.config['UPLOAD_FOLDER'] = "uploads/"
+app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg'])
 
 app.config['MAIL_SERVER'] ='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
@@ -25,6 +25,11 @@ mail = Mail(app)
 #database stuff
 cursor = sqlite3.connect("data/data00.db")
 query = "CREATE TABLE IF NOT EXISTS users WITH VALUES ()"
+
+# For a given file, return whether it's an allowed type or not
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 #mail functions
 #here is async wrapper for mail
@@ -58,15 +63,45 @@ def sendVerificationEmail(email, verificationLink):
 
 @app.route("/", methods = ["GET", "POST"])
 def home():
-    if ['user'] in session:
+    if 'user' in session:
         return render_template("events.html", events = accounts.getEvents(user))
-    else:
-        if 'submit' in request:
+#    else:
+#        if 'submit' in request:
             #replacerino
-            pass
-        else:
-            return render_template("index.html")
+        pass
+    else:
+        return render_template("index.html")
 
+
+# Route that will process the file upload
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the name of the uploaded file
+    f = request.files['file']
+    # Check if the file is one of the allowed types/extensions
+    if f and allowed_file(f.filename):
+        # Make the filename safe, remove unsupported chars
+        filename = secure_filename(f.filename)
+        # Move the file form the temporal folder to
+        # the upload folder we setup
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Redirect the user to the uploaded_file route, which
+        # will basicaly show on the browser the uploaded file
+        return redirect(url_for('uploaded_file',
+                                filename=filename))
+    else:
+        return "bad file!"
+    
+# This route is expecting a parameter containing the name
+# of a file. Then it will locate that file on the upload
+# directory and show it on the browser, so if the user uploads
+# an image, that image is going to be show after the upload
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+    
 if __name__ == "__main__":
     app.debug = True
     app.run()
